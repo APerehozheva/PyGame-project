92afca9387421c3bc4eec7511050e82c05ffe639
Анна <aperekhozheva@bk.ru>
2021-01-04 19:37:20 +0500

    Написан основной игровой цикл. Добавлен и реализован класс "Basket".

--- program.py
+++ program.py
@@ -29,3 +29,45 @@ def load_image(name, colorkey=None):
     else:
         image = image.convert_alpha()
     return image
+
+
+class Basket(pygame.sprite.Sprite):
+    image = load_image("box.png", colorkey=-1)
+
+    def __init__(self):
+        super().__init__(all_sprites)
+        self.image = Basket.image
+        self.image = pygame.transform.scale(self.image, (100, 100))
+
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.bottom = height
+
+    def update(self):
+        global f
+        if f == 'left' and self.rect.x > 0:
+            self.rect = self.rect.move(-100, 0)
+        elif f == 'right' and self.rect.x < 600:
+            self.rect = self.rect.move(100, 0)
+
+
+basket = Basket()
+
+running = True
+while running:
+    screen.fill((0, 0, 0))
+    f = None
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                f = 'left'
+            elif event.key == pygame.K_RIGHT:
+                f = 'right'
+        else:
+            f = None
+    all_sprites.update()
+    all_sprites.draw(screen)
+    pygame.display.flip()
+terminate()
