ddbec4341db7656ee568a9432f70b6ed310937b9
Анна <aperekhozheva@bk.ru>
2021-01-06 13:22:29 +0500

    Добавлен класс "Fruit". Созданы переменные для ведения счёта.

--- program.py
+++ program.py
@@ -1,6 +1,7 @@
 import pygame
 import os
 import sys
+import random
 
 pygame.init()
 size = width, height = 700, 500
@@ -8,6 +9,12 @@ screen = pygame.display.set_mode(size)
 pygame.display.set_caption('HARVESTING')
 
 all_sprites = pygame.sprite.Group()
+
+A = [25, 125, 225, 325, 425, 525, 625]
+tm = pygame.time.Clock()
+time = 200
+score = 0
+omissions = 0
 
 
 def terminate():
@@ -53,7 +60,42 @@ class Basket(pygame.sprite.Sprite):
 
 basket = Basket()
 
+
+class Fruit(pygame.sprite.Sprite):
+    image1 = load_image('apple.png')
+    image2 = load_image('pear.png')
+    image3 = load_image('apricot.png')
+
+    def __init__(self, pos):
+        super().__init__(all_sprites)
+        self.images = [(Fruit.image1, (45, 60)), (Fruit.image2, (45, 65)), (Fruit.image3, (50, 48))]
+        im = random.choice(self.images)
+        self.image = im[0]
+        self.image = pygame.transform.scale(self.image, im[1])
+
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+    def update(self):
+        global score, omissions, time
+        if not pygame.sprite.collide_mask(self, basket) and self.rect.colliderect(screen.get_rect()):
+            self.rect = self.rect.move(0, 1)
+        elif pygame.sprite.collide_mask(self, basket):
+            score += 1
+            self.kill()
+            Fruit((random.choice(A), 0))
+            if time < 400:
+                time += 2
+        else:
+            omissions += 1
+            self.kill()
+            Fruit((random.choice(A), 0))
+
+
 running = True
+Fruit((random.choice(A), 0))
 while running:
     screen.fill((0, 0, 0))
     f = None
@@ -69,5 +111,6 @@ while running:
             f = None
     all_sprites.update()
     all_sprites.draw(screen)
+    tm.tick(time)
     pygame.display.flip()
 terminate()
